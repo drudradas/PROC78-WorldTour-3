07bc9a4a10b006b46c700b65a630dbf580afe202
drudradas <72086752+drudradas@users.noreply.github.com>
2021-05-12 11:33:44 +0530

    Update WorldMap.js

--- screens/WorldMap.js
+++ screens/WorldMap.js
@@ -22,7 +22,7 @@ export default class WorldMap extends Component {
     }
     render() {
         const { longitude, latitude } = this.state;
-        const path = `https://virtualsky.lco.global/embed/index.html?longitude=${longitude}&latitude=${latitude}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true&projection=stereo&showdate=false&showposition=false`
+    const path = `https://www.openstreetmap.org/#map=13/${latitude}/${longitude}`
         return (
             <View style={{ flex: 1, backgroundColor: "#A8DADC" }}>
                 <SafeAreaView style={styles.droidSafeArea} />
